c0ece841f96b7e846c0301dde3894ad86ea3f623
Andrzej Kebab <grzesiugora123@wp.pl>
2022-12-13 15:08:22 +0100

    em idk

---
+++ RPG IG/Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -0,0 +1,49 @@
+using PatataStudio.Inputs;
+using UnityEngine;
+
+namespace PatataStudio.DialogueSystem
+{
+	public class DialogueTrigger : MonoBehaviour
+	{
+		[Header("Visual Cue")]
+		[SerializeField] private GameObject visualCue;
+
+		[Header("Ink JSON")]
+		[SerializeField] private TextAsset inkJSON;
+		
+		private bool playerInRange;
+
+		private void Awake()
+		{
+			playerInRange = false;
+			visualCue.SetActive(false);
+		}
+
+		private void Update()
+		{
+			if (playerInRange && !DialogueManager.instance.dialogueIsPlaying)
+			{
+				visualCue.SetActive(true);
+				if (InputManager.instance.GetInteractPressed())
+				{
+					DialogueManager.instance.EnterDialogueMode(inkJSON);
+
+				}
+			}
+			else
+			{
+				visualCue.SetActive(false);
+			}
+		}
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if (collision.gameObject.tag == "Player") playerInRange = true;
+		}
+
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (collision.gameObject.tag == "Player") playerInRange = false;
+		}
+	}
+}
---
+++ RPG IG/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using PatataStudio.Inputs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	private Camera cam;
+	private Rigidbody2D playerRB;
+	[SerializeField] private Transform rotationPoint;
+	[SerializeField] private Transform firePoint;
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private Slider healtBar;
+
+	private Vector2 movement;
+	private Vector2 dir;
+	private Vector2 cursorPos;
+
+	private int health;
+	[SerializeField] private int maxHealth = 250;
+	[SerializeField] private float moveSpeed = 10f;
+	[SerializeField] private float bulletForce = 20f;
+	[SerializeField] private float startTimeBtwShoots;
+	private float timeBtwShoots;
+
+	void Start()
+	{
+		movement = InputManager.instance.GetMovement();
+		playerRB = GetComponent<Rigidbody2D>();
+		cam = Camera.main;
+
+		health = maxHealth;
+		healtBar.maxValue = maxHealth;
+	}
+
+	void Update()
+	{
+		Vector2 objPos = cam.WorldToScreenPoint(transform.position);
+		dir = (cursorPos - objPos).normalized;
+
+		healtBar.value = health;
+
+		if (Input.GetKeyDown(KeyCode.G)) health -= 10;
+	}
+
+	void FixedUpdate()
+	{
+		playerRB.velocity = new Vector2( movement.x * moveSpeed, movement.y * moveSpeed);
+
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		rotationPoint.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+		if (timeBtwShoots <= 0)
+		{
+			if (InputManager.instance.GetFirePressed() == 1)
+			{
+				Shoot();
+				timeBtwShoots = startTimeBtwShoots;
+			}
+		}
+		else
+		{
+			timeBtwShoots -= Time.deltaTime;
+		}
+	}
+
+	void Shoot()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+		bulletRB.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+		Destroy(bullet, 5f);
+	}
+}
